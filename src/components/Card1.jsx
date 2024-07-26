913914c365270ece3a517f9d4950b521692e449c
gymcoding <bruce.lean17@gmail.com>
2024-07-26 16:43:13 +0900

    5.3 기존 CSS 문제점

---
+++ src/components/Card1.jsx
@@ -0,0 +1,6 @@
+import './Card1.css';
+
+function Card1() {
+  return <article className="card">Card1</article>;
+}
+export default Card1;
---
+++ src/components/Card2.jsx
@@ -0,0 +1,6 @@
+import './Card2.css';
+
+function Card2() {
+  return <article className="card">Card2</article>;
+}
+export default Card2;
