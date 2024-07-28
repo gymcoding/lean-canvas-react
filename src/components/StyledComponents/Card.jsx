23bfa00ac9fe31600f320a060cbc61336fc4c7be
gymcoding <bruce.lean17@gmail.com>
2024-07-28 13:14:53 +0900

    6.3 실습 - Styled Components

--- src/components/StyledComponents/Card.jsx
+++ src/components/StyledComponents/Card.jsx
@@ -1,11 +1,17 @@
-import styles from './Card.module.css';
+import styled from 'styled-components';
+
+const CardComponent = styled.div`
+  border: 2px solid #393939;
+  padding: 24px;
+  border-radius: 6px;
+`;
 
 function Card() {
   return (
-    <div className={styles['card-container']}>
+    <CardComponent>
       <h2>Styled Component</h2>
       <p>이것은 styled-components로 만든 카드입니다.</p>
-    </div>
+    </CardComponent>
   );
 }
 
