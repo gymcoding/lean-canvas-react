d597ce94707e39bd4a7dd25eb7026ffcfe08817d
gymcoding <bruce.lean17@gmail.com>
2024-07-28 13:19:04 +0900

    6.4 실습 - props dark 설정

--- src/components/StyledComponents/Card.jsx
+++ src/components/StyledComponents/Card.jsx
@@ -4,11 +4,22 @@ const CardComponent = styled.div`
   border: 2px solid #393939;
   padding: 24px;
   border-radius: 6px;
+  ${props => {
+    console.log('props: ', props);
+    return (
+      props.dark &&
+      `
+      background-color: black;
+      color: white;
+      border: none;
+    `
+    );
+  }}
 `;
 
 function Card() {
   return (
-    <CardComponent>
+    <CardComponent dark>
       <h2>Styled Component</h2>
       <p>이것은 styled-components로 만든 카드입니다.</p>
     </CardComponent>
