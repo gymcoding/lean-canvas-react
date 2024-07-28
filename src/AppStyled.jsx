4df9326510698406604e9f655a37265f3b1b1a66
gymcoding <bruce.lean17@gmail.com>
2024-07-28 13:32:28 +0900

    6.8 실습 - 리액트 컴포넌트 확장

--- src/AppStyled.jsx
+++ src/AppStyled.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Card from './components/StyledComponents/Card';
-
+import BaseButton from './components/StyledComponents/BaseButton';
+import StyledButton from './components/StyledComponents/StyledButton';
 function App() {
   return (
     <>
       <Card />
+      <hr />
+      <BaseButton>BaseButton</BaseButton>
+      <StyledButton>StyledButton</StyledButton>
     </>
   );
 }
