a072faacbbf732885fcbf38cc0089dcdcd2f3ed9
gymcoding <bruce.lean17@gmail.com>
2024-08-16 10:53:46 +0900

    19.3 수정 모드 추가

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { AiOutlineClose, AiOutlineCheck } from 'react-icons/ai';
 
 const Note = ({ id, onRemoveNote }) => {
@@ -8,35 +9,53 @@ const Note = ({ id, onRemoveNote }) => {
     'bg-green-300',
   ];
 
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
-    <div className={`p-4 bg-yellow-300 relative max-h-[32rem] overflow-hidden`}>
+    <div
+      className={`p-4 bg-yellow-300 relative max-h-[32rem] overflow-hidden`}
+      onClick={() => setIsEditing(true)}
+    >
       <div className="absolute top-2 right-2">
-        <button aria-label="Check Note" className="text-gray-700">
-          <AiOutlineCheck size={20} />
-        </button>
-        <button
-          aria-label="Close Note"
-          className="text-gray-700"
-          onClick={() => onRemoveNote(id)}
-        >
-          <AiOutlineClose size={20} />
-        </button>
+        {isEditing ? (
+          <button
+            aria-label="Check Note"
+            className="text-gray-700"
+            onClick={e => {
+              e.stopPropagation();
+              setIsEditing(false);
+            }}
+          >
+            <AiOutlineCheck size={20} />
+          </button>
+        ) : (
+          <button
+            aria-label="Close Note"
+            className="text-gray-700"
+            onClick={() => onRemoveNote(id)}
+          >
+            <AiOutlineClose size={20} />
+          </button>
+        )}
       </div>
       <textarea
         className={`w-full h-full bg-transparent resize-none border-none focus:outline-none text-gray-900 overflow-hidden`}
         aria-label="Edit Note"
         placeholder="메모를 작성하세요."
         style={{ height: 'auto', minHeight: '8rem' }}
+        readOnly={!isEditing}
       />
-      <div className="flex space-x-2">
-        {colorOptions.map((option, index) => (
-          <button
-            key={index}
-            className={`w-6 h-6 rounded-full cursor-pointer outline outline-gray-50 ${option}`}
-            aria-label={`Change color to ${option}`}
-          />
-        ))}
-      </div>
+      {isEditing && (
+        <div className="flex space-x-2">
+          {colorOptions.map((option, index) => (
+            <button
+              key={index}
+              className={`w-6 h-6 rounded-full cursor-pointer outline outline-gray-50 ${option}`}
+              aria-label={`Change color to ${option}`}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
