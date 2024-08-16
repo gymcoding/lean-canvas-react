649f1caec1be3af4e00e18c6fa84645a003dd6df
gymcoding <bruce.lean17@gmail.com>
2024-08-16 10:38:41 +0900

    19.2 Note 컴포넌트 마크업

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,44 @@
-import { AiOutlineClose } from 'react-icons/ai';
+import { AiOutlineClose, AiOutlineCheck } from 'react-icons/ai';
 
-function Note({ id, onRemoveNote }) {
+const Note = ({ id, onRemoveNote }) => {
+  const colorOptions = [
+    'bg-yellow-300',
+    'bg-pink-300',
+    'bg-blue-300',
+    'bg-green-300',
+  ];
+
   return (
-    <div className="border border-black">
-      <button onClick={() => onRemoveNote(id)}>
-        <AiOutlineClose size={20} />
-      </button>
+    <div className={`p-4 bg-yellow-300 relative max-h-[32rem] overflow-hidden`}>
+      <div className="absolute top-2 right-2">
+        <button aria-label="Check Note" className="text-gray-700">
+          <AiOutlineCheck size={20} />
+        </button>
+        <button
+          aria-label="Close Note"
+          className="text-gray-700"
+          onClick={() => onRemoveNote(id)}
+        >
+          <AiOutlineClose size={20} />
+        </button>
+      </div>
+      <textarea
+        className={`w-full h-full bg-transparent resize-none border-none focus:outline-none text-gray-900 overflow-hidden`}
+        aria-label="Edit Note"
+        placeholder="메모를 작성하세요."
+        style={{ height: 'auto', minHeight: '8rem' }}
+      />
+      <div className="flex space-x-2">
+        {colorOptions.map((option, index) => (
+          <button
+            key={index}
+            className={`w-6 h-6 rounded-full cursor-pointer outline outline-gray-50 ${option}`}
+            aria-label={`Change color to ${option}`}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Note;
