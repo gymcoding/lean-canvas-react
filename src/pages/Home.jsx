2d8eac92dc4103ec6a21469897ca6bb7e56fc301
gymcoding <bruce.lean17@gmail.com>
2024-08-21 12:44:59 +0900

    24 등록구현, dayjs 패키지, Button 컴포넌트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
-import { getCanvases } from '../api/canvas';
+import { createCanvas, getCanvases } from '../api/canvas';
 
 import CanvasList from '../components/CanvasList';
 import SearchBar from '../components/SearchBar';
 import ViewToggle from '../components/ViewToggle';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
+import Button from '../components/Button';
+
 function Home() {
   const [searchText, setSearchText] = useState();
   const [isGridView, setIsGridView] = useState(true);
@@ -17,7 +19,7 @@ function Home() {
     try {
       setIsLoading(true);
       setError(null);
-      await new Promise(resolver => setTimeout(resolver, 2000));
+      await new Promise(resolver => setTimeout(resolver, 1000));
       const response = await getCanvases(params);
       setData(response.data);
     } catch (err) {
@@ -35,11 +37,30 @@ function Home() {
     setData(data.filter(item => item.id !== id));
   };
 
+  const [isLoadingCreate, setIsLoadingCreate] = useState(false);
+  const handleCreateCanvas = async () => {
+    try {
+      setIsLoadingCreate(true);
+      await new Promise(resolver => setTimeout(resolver, 1000));
+      await createCanvas();
+      fetchData({ title_like: searchText });
+    } catch (err) {
+      alert(err.message);
+    } finally {
+      setIsLoadingCreate(false);
+    }
+  };
+
   return (
     <>
       <div className="mb-6 flex flex-col sm:flex-row items-center justify-between">
         <SearchBar searchText={searchText} setSearchText={setSearchText} />
         <ViewToggle isGridView={isGridView} setIsGridView={setIsGridView} />
+      </div>
+      <div className="flex justify-end mb-6">
+        <Button onClick={handleCreateCanvas} loading={isLoadingCreate}>
+          등록하기
+        </Button>
       </div>
       {isLoading && <Loading />}
       {error && (
