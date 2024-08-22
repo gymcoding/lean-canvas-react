fd9b6a2a95e7cc8df4291a9bf23e5ee9d5f4c9c1
gymcoding <bruce.lean17@gmail.com>
2024-08-22 17:29:33 +0900

    26.1 상세 조회

--- src/pages/CanvasDetail.jsx
+++ src/pages/CanvasDetail.jsx
@@ -1,8 +1,22 @@
+import { useParams } from 'react-router-dom';
 import CanvasTitle from '../components/CanvasTitle';
 import LeanCanvas from '../components/LeanCanvas';
+import { useEffect, useState } from 'react';
+import { getCanvasById } from '../api/canvas';
 function CanvasDetail() {
+  const { id } = useParams();
+  const [canvas, setCanvas] = useState();
+
+  useEffect(() => {
+    const fetchCanvas = async () => {
+      const data = await getCanvasById(id);
+      setCanvas(data);
+    };
+    fetchCanvas();
+  }, [id]);
   return (
     <div>
+      {JSON.stringify(canvas)}
       <CanvasTitle />
       <LeanCanvas />
     </div>
