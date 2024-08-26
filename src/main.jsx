46a47c6623f95ec3b1432d0cba1a44dda9961c8b
gymcoding <bruce.lean17@gmail.com>
2024-08-26 11:54:43 +0900

    30.1 react-query 설치

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css';
 import App from './App';
 import Home from './pages/Home';
@@ -8,6 +9,8 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import ErrorPage from './pages/ErrorPage';
 import CanvasDetail from './pages/CanvasDetail';
+
+const queryClient = new QueryClient();
 const router = createBrowserRouter([
   {
     path: '/',
@@ -35,6 +38,8 @@ const router = createBrowserRouter([
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
